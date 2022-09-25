127d938c69578fe0572bde5905102473363db615
Marsel Sattarov <fsc-3@tages.ru>
2022-09-25 18:42:25 +0500

    add scrollToTop

---
+++ js/scrollToTop.js
@@ -0,0 +1,11 @@
+const topBtn = document.querySelector('#scrollToTopButton');
+
+topBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+
+   seamless.scrollIntoView(document.querySelector('.header'), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+    });
+})

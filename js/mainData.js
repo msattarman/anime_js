597a69fbe7f5b0f9a112b25226a7d0a43a72c7f7
Marsel Sattarov <fsc-3@tages.ru>
2022-09-27 00:39:29 +0500

    get ganres from data

--- js/mainData.js
+++ js/mainData.js
@@ -1,8 +1,63 @@
 const mainData = () => {
 
    const renderAnimeList = (array, ganres) => {
-      console.log(array)
-      console.log(ganres)
+      const wrapper = document.querySelector(".product .col-lg-8");
+      
+      wrapper.innerHTML = ''
+
+      ganres.forEach((ganre) => {
+         const productBlock = document.createElement('div')
+         const listBlock = document.createElement('div')
+         const list = array.filter(item => item.ganre === ganre)
+
+         listBlock.classList.add('row')
+         productBlock.classList.add('mb-5')
+
+         productBlock.insertAdjacentHTML("afterbegin",`
+            <div class="row">
+               <div class="col-lg-8 col-md-8 col-sm-8">
+                   <div class="section-title">
+                       <h4>${ganre}</h4>
+                   </div>
+               </div>
+               <div class="col-lg-4 col-md-4 col-sm-4">
+                   <div class="btn__all">
+                       <a href="/categories.html" class="primary-btn">View All <class="arrow_right"></class=></a>
+                  </div>
+               </div>
+            </div>
+         `);
+
+         list.forEach(item => {
+            listBlock.insertAdjacentHTML(
+              "afterbegin",
+              `
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                  <div class="product__item">
+                     <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                        <div class="ep">${item.rating} / 10</div>
+                        <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+                     </div>
+                     <div class="product__item__text">
+                        <ul>
+                           <li>Active</li>
+                           <li>Movie</li>
+                        </ul>
+                        <h5><a href="/anime-details.html">${item.title}</a></h5>
+                     </div>
+                  </div>
+               </div>
+            
+            `);
+         })
+
+         productBlock.append(listBlock)
+         wrapper.append(productBlock)
+
+         wrapper.querySelectorAll(".set-bg").forEach((elem) => {
+           elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+         });
+      })
    }
 
    const renderTopAnime = (array) => {
@@ -11,7 +66,6 @@ const mainData = () => {
       wrapper.innerHTML = ''
 
       array.forEach((item) => {
-         console.log(item);
          wrapper.insertAdjacentHTML("afterbegin",
            `
             <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
